9a668b78c4ed659d1e25502530d0414a749fc4df
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-08-09 13:39:13 +0530

    Create StdinAndStdout.java

---
+++ DSA/StdinAndStdout.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Solution{
+public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int a = scanner.nextInt();
+    double b = scanner.nextDouble();
+    scanner.nextLine();
+    String c = scanner.nextLine();
+
+    System.out.println("String: "+c);
+    System.out.println("Double: "+b);
+    System.out.println("Int: "+a);
+
+}
+}
