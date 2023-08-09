8b195c5f3651c63923297cd3e1ec78474ec91c46
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-08-09 13:19:50 +0530

    Create SolveMe.java

---
+++ DSA/SolveMe.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Solution {
+
+ public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a;
+        a = scanner.nextInt();
+        int b;
+        b = scanner.nextInt();
+        int sum;
+        sum = a+b;
+        System.out.println(sum);
+	}
+}
