2701efc3a5f9fdcda7f03f8f62eb82559a227401
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-10-07 16:27:07 +0530

    Create IntToString.java

---
+++ DSA/IntToString.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Solution {
+
+public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    if( n>= -100 && n<= 100)
+    {
+        String s=Integer.toString(n);
+        System.out.println("Good job");
+    }
+    else
+    {
+        System.out.println("Wrong answer");
+    }
+}
+}
